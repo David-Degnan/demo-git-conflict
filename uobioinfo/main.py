a71c6d524f03e533996b1d38662ea445c477acfe
David-Degnan <ddegnan@uoregon.edu>
2018-10-11 13:21:04 -0700

    BetterProgram

--- uobioinfo/main.py
+++ uobioinfo/main.py
@@ -3,10 +3,9 @@ import argparse
 
 def say_something(phrase): # ... I'm giving up on you. 
     """Change this however you'd like!"""
-    print("testphrase1")
     print(phrase)
-    print("testphrase2")
-
+    print("Jet fuel can't melt steel beams.") 
+    
 
 # --- Don't change anything below this line -------
 
